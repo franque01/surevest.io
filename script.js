7245b76b844f04e15acc7d847a5e3ea2bc60fa28
Frank Okeke <franquechuks@gmail.com>
2023-12-26 02:26:41 +0100

    commit

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const menuBtn = document.getElementById('menu-bar');
 const closeBtn = document.querySelector('.close-btn');
+const links = document.querySelectorAll('.links');
 
-// console.log(menu)
+var menuBar1 = document.querySelector('.menu_layer_1');
+var menuBar2 = document.querySelector('.menu_layer_2');
+
+console.log(links)
 menuBtn.addEventListener('click', function(){
-    var menuBar1 = document.querySelector('.menu_layer_1');
-    var menuBar2 = document.querySelector('.menu_layer_2');
-
-    menuBar1.classList.toggle('display')
-    // if(menuBar1.classList.contains('display')){
-    //     menuBar1.classList.remove('display');
-    // }else{
-    //     menuBar1.classList.add('display');
-    // }
+    menuBar1.classList.toggle('display');
     menuBar2.classList.toggle('display');
-    // if(menuBar2.classList.contains('display')){
-    //     menuBar2.classList.remove('display');
-    // }else{
-    //     menuBar2.classList.add('display');
-    // }
-})
-closeBtn.addEventListener('click', function(){
-    var menuBar2 = document.querySelector('.menu_layer_2');
 
+});
+
+closeBtn.addEventListener('click', function(){
     menuBar2.classList.toggle('display')
-})
+});
+
+links.forEach(link => {
+    link.addEventListener('click', function(){
+        menuBar2.classList.toggle('display');
+    })
+});
